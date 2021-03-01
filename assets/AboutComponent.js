520483138235b711fdda54fa7e864e282f0f4d37
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-03-01 13:39:42 +0200

    initial commit, with focus on mobile

---
+++ assets/AboutComponent.js
@@ -0,0 +1,272 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const AboutComponent = ({ svgRef }) => (
+  <svg width={375} height={812} viewBox="0 0 375 812" ref={svgRef}>
+    <defs>
+      <style>
+        {
+          ".a,.e,.j,.m{fill:none;}.b{clip-path:url(#i);}.c{opacity:0.8;fill:url(#a);}.d,.t,.v{fill:#fff;}.d{opacity:0.001;}.e{stroke:#000;stroke-miterlimit:10;opacity:0.35;}.f{opacity:0.4;}.g{font-size:15px;font-family:SegoeUI-Semibold, Segoe UI;font-weight:600;letter-spacing:-0.02em;}.h{fill:rgba(233,192,233,0.7);}.h,.i,.k,.o{font-size:20px;}.h,.i,.o,.p,.s,.t{font-family:CoolveticaRg-Regular, Coolvetica;}.h,.i,.k,.m,.o{letter-spacing:0.04em;}.i,.n{fill:#e9c0e9;}.j{stroke:#e9c0e9;stroke-linecap:round;stroke-width:4px;}.k,.o,.s,.u{fill:#14012d;}.k,.m{font-family:LEMONMILK-Bold, LEMON MILK;font-weight:700;}.l{opacity:0.5;}.m{stroke:#fff;font-size:72px;}.p{fill:#faeafe;font-size:16px;}.p,.s,.t{letter-spacing:0.054em;}.q{fill:url(#f);}.r{fill:url(#g);}.s{font-size:14px;}.t{font-size:36px;}.w{fill:rgba(20,1,45,0.4);}.x{fill:#d35be1;}.y{stroke:none;}.z{fill:#f1cefa;}.aa{filter:url(#d);}.ab{filter:url(#b);}"
+        }
+      </style>
+      <pattern id="a" width={1} height={1} viewBox="157.191 0 375 812">
+        <image
+          preserveAspectRatio="xMidYMid slice"
+          width={541.383}
+          height={812}
+          xlinkHref="ComponentTMP_0-image2.jpg"
+        />
+      </pattern>
+      <filter
+        id="b"
+        x={98}
+        y={625}
+        width={174}
+        height={66}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dx={-3} dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={5} result="c" />
+        <feFlood floodColor="#14012d" floodOpacity={0.2} />
+        <feComposite operator="in" in2="c" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="d"
+        x={161}
+        y={277}
+        width={208}
+        height={322}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dx={-3} dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={5} result="e" />
+        <feFlood floodColor="#14012d" floodOpacity={0.502} />
+        <feComposite operator="in" in2="e" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <pattern
+        id="f"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 800 1200"
+      >
+        <image
+          width={800}
+          height={1200}
+          xlinkHref="ComponentTMP_0-image3.png"
+        />
+      </pattern>
+      <pattern
+        id="g"
+        width={1}
+        height={1}
+        viewBox="-37.268 -8.473 308.237 85.621"
+      >
+        <image
+          preserveAspectRatio="xMidYMid slice"
+          width={216}
+          height={76.842}
+          xlinkHref="ComponentTMP_0-image4.png"
+        />
+      </pattern>
+      <clipPath id="i">
+        <rect width={375} height={812} />
+      </clipPath>
+    </defs>
+    <g id="h" className="b">
+      <rect className="z" width={375} height={812} />
+      <rect className="c" width={375} height={812} />
+      <rect className="d" width={375} height={44} />
+      <g transform="translate(21 13)">
+        <g transform="translate(315.672 3.333)">
+          <g className="e">
+            <rect className="y" width={22} height={11.333} rx={2.667} />
+            <rect
+              className="a"
+              x={0.5}
+              y={0.5}
+              width={21}
+              height={10.333}
+              rx={2.167}
+            />
+          </g>
+          <path
+            className="f"
+            d="M0,0V4A2.17,2.17,0,0,0,1.328,2,2.17,2.17,0,0,0,0,0"
+            transform="translate(23 3.667)"
+          />
+          <rect
+            width={18}
+            height={7.333}
+            rx={1.333}
+            transform="translate(2 2)"
+          />
+        </g>
+        <path
+          d="M7.667,11a.314.314,0,0,1-.222-.093L5.439,8.885a.31.31,0,0,1-.094-.231.316.316,0,0,1,.1-.227,3.437,3.437,0,0,1,4.437,0,.315.315,0,0,1,.009.458L7.889,10.907A.314.314,0,0,1,7.667,11ZM11.19,7.451a.313.313,0,0,1-.215-.086,4.928,4.928,0,0,0-6.612,0,.313.313,0,0,1-.215.086.308.308,0,0,1-.22-.091L2.768,6.189a.322.322,0,0,1,0-.46,7.192,7.192,0,0,1,9.791,0,.323.323,0,0,1,.1.23.319.319,0,0,1-.095.23L11.409,7.36A.309.309,0,0,1,11.19,7.451Zm2.669-2.693a.3.3,0,0,1-.215-.088,8.68,8.68,0,0,0-11.955,0,.307.307,0,0,1-.435,0L.094,3.5a.323.323,0,0,1,0-.457,10.948,10.948,0,0,1,15.141,0,.323.323,0,0,1,0,.457l-1.161,1.17A.306.306,0,0,1,13.859,4.758Z"
+          transform="translate(295.339 3.331)"
+        />
+        <path
+          d="M16,10.667H15a1,1,0,0,1-1-1V1a1,1,0,0,1,1-1h1a1,1,0,0,1,1,1V9.667A1,1,0,0,1,16,10.667Zm-4.667,0h-1a1,1,0,0,1-1-1V3.334a1,1,0,0,1,1-1h1a1,1,0,0,1,1,1V9.667A1,1,0,0,1,11.334,10.667Zm-4.667,0h-1a1,1,0,0,1-1-1v-4a1,1,0,0,1,1-1h1a1,1,0,0,1,1,1v4A1,1,0,0,1,6.666,10.667ZM2,10.667H1a1,1,0,0,1-1-1v-2a1,1,0,0,1,1-1H2a1,1,0,0,1,1,1v2A1,1,0,0,1,2,10.667Z"
+          transform="translate(273.339 3.667)"
+        />
+        <text className="g">
+          <tspan x={14.116} y={16}>
+            {"9:41"}
+          </tspan>
+        </text>
+      </g>
+      <text className="h" transform="translate(300 139)">
+        <tspan x={0} y={0}>
+          {"Lineup"}
+        </tspan>
+      </text>
+      <text className="i" transform="translate(300 101)">
+        <tspan x={0} y={0}>
+          {"About"}
+        </tspan>
+      </text>
+      <line className="j" x2={24} transform="translate(260 94)" />
+      <g transform="translate(16 56)">
+        <text className="k" transform="translate(0 22)">
+          <tspan x={0} y={0}>
+            {"LITXP"}
+          </tspan>
+        </text>
+      </g>
+      <g className="l" transform="translate(160 743)">
+        <text className="m" transform="translate(0 78)">
+          <tspan x={0} y={0}>
+            {"LITXP"}
+          </tspan>
+        </text>
+      </g>
+      <g transform="translate(116 637)">
+        <g className="ab" transform="matrix(1, 0, 0, 1, -116, -637)">
+          <rect
+            className="n"
+            width={144}
+            height={36}
+            rx={10}
+            transform="translate(116 637)"
+          />
+        </g>
+        <text className="o" transform="translate(49.5 25)">
+          <tspan x={0} y={0}>
+            {"RSVP"}
+          </tspan>
+        </text>
+      </g>
+      <g className="aa" transform="matrix(1, 0, 0, 1, 0, 0)">
+        <text className="p" transform="translate(179 289)">
+          <tspan x={0} y={15}>
+            {"Join us for the first "}
+          </tspan>
+          <tspan x={0} y={34}>
+            {"ever LIT XP "}
+          </tspan>
+          <tspan x={0} y={53} />
+          <tspan x={0} y={72}>
+            {"Take a beat and spend "}
+          </tspan>
+          <tspan x={0} y={91}>
+            {"a day with good "}
+          </tspan>
+          <tspan x={0} y={110}>
+            {"people, vibey vibes and "}
+          </tspan>
+          <tspan x={0} y={129}>
+            {"live music that's sure "}
+          </tspan>
+          <tspan x={0} y={148}>
+            {"to leave you lost in a "}
+          </tspan>
+          <tspan x={0} y={167}>
+            {"trance. "}
+          </tspan>
+          <tspan x={0} y={186} />
+          <tspan x={0} y={205}>
+            {"RSVP by hitting the "}
+          </tspan>
+          <tspan x={0} y={224}>
+            {"button and fill out the "}
+          </tspan>
+          <tspan x={0} y={243}>
+            {"form."}
+          </tspan>
+          <tspan x={0} y={262} />
+          <tspan x={0} y={281}>
+            {"See you there."}
+          </tspan>
+        </text>
+      </g>
+      <rect
+        className="q"
+        width={48}
+        height={74}
+        transform="translate(69 691)"
+      />
+      <rect
+        className="r"
+        width={216}
+        height={60}
+        transform="translate(133 691)"
+      />
+      <text className="s" transform="translate(16 165)">
+        <tspan x={0} y={0}>
+          {"TMC presents"}
+        </tspan>
+      </text>
+      <text className="t" transform="translate(188 232)">
+        <tspan x={-138.762} y={0}>
+          {"L"}
+        </tspan>
+        <tspan className="u" y={0}>
+          {"ost "}
+        </tspan>
+        <tspan y={0}>{"I"}</tspan>
+        <tspan className="u" y={0}>
+          {"n a "}
+        </tspan>
+        <tspan y={0}>{"T"}</tspan>
+        <tspan className="u" y={0}>
+          {"rance "}
+        </tspan>
+        <tspan className="u">
+          <tspan x={-92.412} y={30}>
+            {"e"}
+          </tspan>
+          <tspan className="v" y={30}>
+            {"XP"}
+          </tspan>
+          <tspan y={30}>{"erience"}</tspan>
+        </tspan>
+      </text>
+      <circle
+        className="w"
+        cx={18}
+        cy={18}
+        r={18}
+        transform="translate(315 636)"
+      />
+      <path
+        className="x"
+        d="M15,9l-2.115,2.115L19.755,18l-6.87,6.885L15,27l9-9Z"
+        transform="translate(316.115 636)"
+      />
+    </g>
+  </svg>
+);
+
+const ForwardRef = React.forwardRef((props, ref) => (
+  <AboutComponent svgRef={ref} {...props} />
+));
+export default ForwardRef;
---
+++ assets/NoziphoComponent.js
@@ -0,0 +1,227 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const NoziphoComponent = ({ svgRef }) => (
+  <svg width={375} height={812} viewBox="0 0 375 812" ref={svgRef}>
+    <defs>
+      <style>
+        {
+          ".a,.f,.k,.n{fill:none;}.b,.i,.o{fill:#e9c0e9;}.b,.k{stroke:#e9c0e9;}.b{opacity:0.48;}.c{clip-path:url(#h);}.d{opacity:0.8;fill:url(#a);}.e{fill:#fff;opacity:0.001;}.f{stroke:#000;stroke-miterlimit:10;opacity:0.35;}.g{opacity:0.4;}.h{font-size:15px;font-family:SegoeUI-Semibold, Segoe UI;font-weight:600;letter-spacing:-0.02em;}.i,.j,.l,.p{font-size:20px;}.i,.j,.p,.q,.u{font-family:CoolveticaRg-Regular, Coolvetica;}.i,.j,.l,.n,.p{letter-spacing:0.04em;}.j{fill:rgba(233,192,233,0.7);opacity:0.7;}.k{stroke-linecap:round;stroke-width:4px;}.l,.p,.s{fill:#14012d;}.l,.n,.r{font-family:LEMONMILK-Bold, LEMON MILK;font-weight:700;}.m,.s{opacity:0.5;}.n{stroke:#fff;font-size:72px;}.q{fill:#faeafe;}.q,.u{font-size:16px;letter-spacing:0.054em;}.r,.y{fill:#f1cefa;}.r{font-size:60px;letter-spacing:0.02em;}.t{clip-path:url(#f);}.u,.w{fill:#d35be1;}.v{fill:rgba(20,1,45,0.4);}.x{stroke:none;}.z{filter:url(#d);}.aa{filter:url(#b);}"
+        }
+      </style>
+      <pattern id="a" width={1} height={1} viewBox="194.984 0 375 812">
+        <image
+          preserveAspectRatio="xMidYMid slice"
+          width={704.967}
+          height={812}
+          xlinkHref="ComponentTMP_2-image2.jpg"
+        />
+      </pattern>
+      <filter
+        id="b"
+        x={98}
+        y={624}
+        width={174}
+        height={66}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dx={-3} dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={5} result="c" />
+        <feFlood floodColor="#14012d" floodOpacity={0.2} />
+        <feComposite operator="in" in2="c" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="d"
+        x={115}
+        y={483}
+        width={257}
+        height={159}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dx={-3} dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={5} result="e" />
+        <feFlood floodColor="#14012d" floodOpacity={0.502} />
+        <feComposite operator="in" in2="e" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <clipPath id="f">
+        <circle
+          className="b"
+          cx={18}
+          cy={18}
+          r={18}
+          transform="translate(935 360)"
+        />
+      </clipPath>
+      <clipPath id="h">
+        <rect width={375} height={812} />
+      </clipPath>
+    </defs>
+    <g id="g" className="c">
+      <rect className="y" width={375} height={812} />
+      <rect className="d" width={375} height={812} />
+      <rect className="e" width={375} height={44} />
+      <g transform="translate(21 13)">
+        <g transform="translate(315.672 3.333)">
+          <g className="f">
+            <rect className="x" width={22} height={11.333} rx={2.667} />
+            <rect
+              className="a"
+              x={0.5}
+              y={0.5}
+              width={21}
+              height={10.333}
+              rx={2.167}
+            />
+          </g>
+          <path
+            className="g"
+            d="M0,0V4A2.17,2.17,0,0,0,1.328,2,2.17,2.17,0,0,0,0,0"
+            transform="translate(23 3.667)"
+          />
+          <rect
+            width={18}
+            height={7.333}
+            rx={1.333}
+            transform="translate(2 2)"
+          />
+        </g>
+        <path
+          d="M7.667,11a.314.314,0,0,1-.222-.093L5.439,8.885a.31.31,0,0,1-.094-.231.316.316,0,0,1,.1-.227,3.437,3.437,0,0,1,4.437,0,.315.315,0,0,1,.009.458L7.889,10.907A.314.314,0,0,1,7.667,11ZM11.19,7.451a.313.313,0,0,1-.215-.086,4.928,4.928,0,0,0-6.612,0,.313.313,0,0,1-.215.086.308.308,0,0,1-.22-.091L2.768,6.189a.322.322,0,0,1,0-.46,7.192,7.192,0,0,1,9.791,0,.323.323,0,0,1,.1.23.319.319,0,0,1-.095.23L11.409,7.36A.309.309,0,0,1,11.19,7.451Zm2.669-2.693a.3.3,0,0,1-.215-.088,8.68,8.68,0,0,0-11.955,0,.307.307,0,0,1-.435,0L.094,3.5a.323.323,0,0,1,0-.457,10.948,10.948,0,0,1,15.141,0,.323.323,0,0,1,0,.457l-1.161,1.17A.306.306,0,0,1,13.859,4.758Z"
+          transform="translate(295.339 3.331)"
+        />
+        <path
+          d="M16,10.667H15a1,1,0,0,1-1-1V1a1,1,0,0,1,1-1h1a1,1,0,0,1,1,1V9.667A1,1,0,0,1,16,10.667Zm-4.667,0h-1a1,1,0,0,1-1-1V3.334a1,1,0,0,1,1-1h1a1,1,0,0,1,1,1V9.667A1,1,0,0,1,11.334,10.667Zm-4.667,0h-1a1,1,0,0,1-1-1v-4a1,1,0,0,1,1-1h1a1,1,0,0,1,1,1v4A1,1,0,0,1,6.666,10.667ZM2,10.667H1a1,1,0,0,1-1-1v-2a1,1,0,0,1,1-1H2a1,1,0,0,1,1,1v2A1,1,0,0,1,2,10.667Z"
+          transform="translate(273.339 3.667)"
+        />
+        <text className="h">
+          <tspan x={14.116} y={16}>
+            {"9:41"}
+          </tspan>
+        </text>
+      </g>
+      <text className="i" transform="translate(300 139)">
+        <tspan x={0} y={0}>
+          {"Lineup"}
+        </tspan>
+      </text>
+      <text className="j" transform="translate(300 101)">
+        <tspan x={0} y={0}>
+          {"About"}
+        </tspan>
+      </text>
+      <line className="k" x2={24} transform="translate(260 132)" />
+      <g transform="translate(16 56)">
+        <text className="l" transform="translate(0 22)">
+          <tspan x={0} y={0}>
+            {"LITXP"}
+          </tspan>
+        </text>
+      </g>
+      <g className="m" transform="translate(160 743)">
+        <text className="n" transform="translate(0 78)">
+          <tspan x={0} y={0}>
+            {"LITXP"}
+          </tspan>
+        </text>
+      </g>
+      <g transform="translate(116 636)">
+        <g className="aa" transform="matrix(1, 0, 0, 1, -116, -636)">
+          <rect
+            className="o"
+            width={144}
+            height={36}
+            rx={10}
+            transform="translate(116 636)"
+          />
+        </g>
+        <text className="p" transform="translate(49.5 25)">
+          <tspan x={0} y={0}>
+            {"RSVP"}
+          </tspan>
+        </text>
+      </g>
+      <g className="z" transform="matrix(1, 0, 0, 1, 0, 0)">
+        <text className="q" transform="translate(133 495)">
+          <tspan x={0} y={15}>
+            {"Before I write, I pray to God "}
+          </tspan>
+          <tspan x={0} y={34}>
+            {"to guide and reveal what he "}
+          </tspan>
+          <tspan x={0} y={53}>
+            {"wants me to say to his "}
+          </tspan>
+          <tspan x={0} y={72}>
+            {"people. "}
+          </tspan>
+        </text>
+      </g>
+      <text className="r" transform="translate(30 241)">
+        <tspan x={0} y={0}>
+          {"Nozipho"}
+        </tspan>
+      </text>
+      <g transform="translate(0 696)">
+        <rect className="s" width={375} height={48} />
+        <g className="t" transform="translate(-620 -354)">
+          <g transform="translate(933.974 358.973)">
+            <path
+              className="o"
+              d="M19.027,37.026a18,18,0,1,1,13.014-5.566,18,18,0,0,1-13.014,5.566ZM19.018,2.057a16.962,16.962,0,1,0,.822.019q-.412-.019-.822-.019Z"
+            />
+            <path
+              className="o"
+              d="M19.027,33.469a14.442,14.442,0,0,1,0-28.884c.23,0,.463.006.7.016a14.442,14.442,0,0,1-.7,28.867Zm0-27.855a13.411,13.411,0,1,0,.647.015C19.457,5.619,19.24,5.614,19.027,5.614Z"
+            />
+            <path
+              className="o"
+              d="M15.78,26.166a1.029,1.029,0,0,1-1.029-1.029V12.917a1.029,1.029,0,0,1,1.6-.856l9.168,6.11a1.029,1.029,0,0,1,0,1.711l-9.168,6.11a1.029,1.029,0,0,1-.57.173Zm0-13.249V25.138l9.168-6.11Z"
+            />
+          </g>
+        </g>
+        <text className="u" transform="translate(24 30)">
+          <tspan x={0} y={0}>
+            {"Nozipho - Thando "}
+          </tspan>
+        </text>
+      </g>
+      <circle
+        className="v"
+        cx={18}
+        cy={18}
+        r={18}
+        transform="translate(24 636)"
+      />
+      <path
+        className="w"
+        d="M21.885,9,24,11.115,17.13,18,24,24.885,21.885,27l-9-9Z"
+        transform="translate(21.115 636)"
+      />
+      <circle
+        className="v"
+        cx={18}
+        cy={18}
+        r={18}
+        transform="translate(315 636)"
+      />
+      <path
+        className="w"
+        d="M15,9l-2.115,2.115L19.755,18l-6.87,6.885L15,27l9-9Z"
+        transform="translate(316.115 636)"
+      />
+    </g>
+  </svg>
+);
+
+const ForwardRef = React.forwardRef((props, ref) => (
+  <NoziphoComponent svgRef={ref} {...props} />
+));
+export default ForwardRef;
